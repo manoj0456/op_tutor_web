2cbd046419e3709edeff26c19f9e2a0d604380db
manoj0456 <kasam.manoj456@gmail.com>
2026-06-15 10:06:27 -0500

    refactor: delete ActivityTracker component

--- src/components/shared/ActivityTracker.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useActivityTracker } from '@/hooks/useActivityTracker'
-
-/** Invisible component that drives the activity heartbeat. Mounted in the root layout. */
-export function ActivityTracker() {
-  useActivityTracker()
-  return null
-}
